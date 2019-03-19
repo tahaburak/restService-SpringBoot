529639c10aee21de513b7ca3172980d6fccdf72c
TCTAHKOC <tahaburak.koc@gmail.com>
2019-03-19 15:30:31 +0300

    added spring boot admin

--- src/main/java/com/burak/spring/RestServiceApplication.java
+++ src/main/java/com/burak/spring/RestServiceApplication.java
@@ -1,9 +1,11 @@
 package com.burak.spring;
 
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableAdminServer
 public class RestServiceApplication {
 
 	public static void main(String[] args) {
